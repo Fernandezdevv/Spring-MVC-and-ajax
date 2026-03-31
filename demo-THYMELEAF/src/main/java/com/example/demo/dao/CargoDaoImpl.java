1a6193da32cca3886e40f500669a3d47e6436cfb
andrei.fernandes <andreifernandezdevv@gmail.com>
2026-03-31 11:44:18 -0300

    Fix: ajustando raiz do projeto

--- demo-THYMELEAF/src/main/java/com/example/demo/dao/CargoDaoImpl.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.dao;
-
-import com.example.demo.domain.Cargo;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class CargoDaoImpl  extends AbstractDao<Cargo, Long> implements CargoDao{
-}
